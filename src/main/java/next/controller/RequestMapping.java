f4d654814ee3213c33f4222c1183af3577284f11
yeongunheo <gjduddns@gmail.com>
2020-06-05 20:19:18 +0900

    mappings에 접근할 수 있도록 RequestMapping 클래스에 메소드 추가

--- src/main/java/next/controller/RequestMapping.java
+++ src/main/java/next/controller/RequestMapping.java
@@ -11,11 +11,19 @@ public class RequestMapping {
 		mappings.put("/users/form", new ForwardController("/user/form.jsp"));
 		mappings.put("", new HomeController());
 		mappings.put("/users", new ListUserController());
-		mappings.put("/users/login", new ForwardController("/user/login.jsp"));
-		mappings.put("/users/loginForm", new LoginFormController());
+		mappings.put("/users/login", new LoginController());
+		mappings.put("/users/loginForm", new ForwardController("/user/login.jsp"));
 		mappings.put("/users/logout", new LogoutController());
 		mappings.put("/users/profile", new ProfileController());
 		mappings.put("/users/update", new UpdateUserController());
 		mappings.put("/users/updateForm", new UpdateUserFormController());
+	}
+	
+	public Controller findController(String url) {
+		return mappings.get(url);
+	}
+	
+	void put(String url, Controller controller) {
+		mappings.put(url, controller);
 	}
 }
